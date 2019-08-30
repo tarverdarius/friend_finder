930c51fa680f4ccc4c60b5a5249b0950bbcba852
tarverdarius@gmail.com <Tarverdarius@gmail.com>
2019-08-29 22:06:41 -0400

    work in progress

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+       "name":"LeBron James",
+       "photo":"https://scontent.fphl2-2.fna.fbcdn.net/v/t1.0-9/35988176_10156619032408944_5427881266689081344_n.jpg?_nc_cat=1&_nc_oc=AQmyM53A_H-K7ikfI7RW_W_jOzf6c-ISlAtReU7-3SzGDh0nthG2dlHd9-7VmAz6WDjQHFUO4igcIhGQSm2-prah&_nc_ht=scontent.fphl2-2.fna&oh=4349e849d36439437e70806001dc35e2&oe=5E02ACC2",
+       "scores":[
+          "5",
+          "1",
+          "4",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "4",
+          "1"
+       ]
+    },
+    {
+       "name":"Seth Rogen",
+       "photo":"htthttps://scontent.fphl2-2.fna.fbcdn.net/v/t1.0-9/12004918_433070620219441_1600627073106205880_n.jpg?_nc_cat=101&_nc_oc=AQlq-05VhzaDM-7PhSFfbgNY0XUiFnKe0TN5RWq9b5SSfSRjdJN-9ptVJBNJ9CKU1gUbWWQ3IdOWGdrIhas78RM4&_nc_ht=scontent.fphl2-2.fna&oh=c04a4fca0b5f37807ffb3c8b34054722&oe=5DCE6E7Bps://www.facebook.com/sethrogen/photos/a.138444186348754/433070620219441/?type=3&theater",
+       "scores":[
+          "4",
+          "2",
+          "5",
+          "1",
+          "3",
+          "2",
+          "2",
+          "1",
+          "3",
+          "2"
+       ]
+    },
+    {
+       "name":"Eddie Murphy",
+       "photo":"https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/269649_163338563739967_7010057_n.jpg?_nc_cat=100&_nc_oc=AQm9-Mcrp8HjVPwwvoPb5KUmMJaCgVRnF9ucdrKpaEpLquwUB-6NZfSdgWqULOBaliLeDDCPiOcHyKSkDT5wFRY5&_nc_ht=scontent.fphl2-1.fna&oh=7616f6a4176bbfa124c4eb7b22dbda09&oe=5E016824",
+       "scores":[
+          "5",
+          "2",
+          "2",
+          "2",
+          "4",
+          "1",
+          "3",
+          "2",
+          "5",
+          "5"
+       ]
+    },
+    {
+       "name":"Kevin Hart",
+       "photo":"https://scontent.fbos1-1.fna.fbcdn.net/v/t1.0-0/p206x206/29135938_127910068042218_3944259206784495934_n.jpg?_nc_cat=111&_nc_oc=AQmOvILNBF9ibAtTyZabzmjpCuAcr5D_sjipcNuotWTrin-ijuzFVA8AL8xbpkLf-FKOfHgJ-GEYVDbbwEnmFINe&_nc_ht=scontent.fbos1-1.fna&oh=5e922557a24aa4129d76f0f3dcef0fe5&oe=5E00329E",
+       "scores":[
+          "3",
+          "3",
+          "4",
+          "2",
+          "2",
+          "1",
+          "3",
+          "2",
+          "2",
+          "3"
+       ]
+    },
+     {
+        "name":"Giannis Antetokounmpo",
+        "photo":"https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/25399187_1527309070723007_456224379796097067_n.jpg?_nc_cat=103&_nc_oc=AQkp1OfB2-zTWUdFp6jJbP3-H_UIrommKh3qDbUoOdR2loZui-HJdR09cBu2eBxA9zs60sLx_GPs3vkPTJuFXDWf&_nc_ht=scontent.fphl2-1.fna&oh=fbd541d0dc69e92bdabe874dcd457323&oe=5E025689",
+        "scores":[
+           "4",
+           "3",
+           "4",
+           "1",
+           "5",
+           "2",
+           "5",
+           "3",
+           "1",
+           "4"
+        ]
+     },
+     {
+        "name":"Barack Obama",
+        "photo":"https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/52025737_10156495813721749_5509645650024726528_n.jpg?_nc_cat=104&_nc_oc=AQllFB6uypMFZ21Tnj8VRtQznRxlKFSknztvGIGlDDR0ocfUeOcOKKSYuvDUu3HGEvX0pVghBwVbVf4-XeSt7V1i&_nc_ht=scontent.fphl2-1.fna&oh=dccf568811c3f613e0d52d8c89a2c2e1&oe=5E1542B6",
+        "scores":[
+           "4",
+           "4",
+           "2",
+           "3",
+           "2",
+           "2",
+           "3",
+           "2",
+           "4",
+           "5"
+        ]
+     }
+ 
+ ]
+ 
+ // Here we export the array. This makes it accessible to other files using require. 
+ module.exports = friends;
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,22 @@
+
+var path = require('path');
+
+
+
+
+
+
+module.exports = function (app) {
+
+
+
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../FRIEND_FINDER/survey.html'));
+    });
+
+
+    app.use(function (req, res) {
+        res.sendFile(path.join(__dirname + './app/public/home.html'));
+    });
+
+}
